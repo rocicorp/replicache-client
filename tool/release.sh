699fb19af6ca77b21727115c0d474a3a547c431c
Aaron Boodman <aaron@aaronboodman.com>
2020-02-23 22:23:22 -1000

    fix version stamp

--- tool/release.sh
+++ tool/release.sh
@@ -13,8 +13,8 @@ echo "Building repl..."
 
 REPL_VERSION=`git describe --tags`
 cd $ROOT
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X roci.dev/replicant/util/version.v=$REPL_VERSION" -o build/repl-darwin-amd64 ./cmd/repl
-GOOS=linux GOARCH=amd64 go build -ldflags "-X roci.dev/replicant/util/version.v=$REPL_VERSION" -o build/repl-linux-amd64 ./cmd/repl
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X roci.dev/diff-server/util/version.v=$REPL_VERSION" -o build/repl-darwin-amd64 ./cmd/repl
+GOOS=linux GOARCH=amd64 go build -ldflags "-X roci.dev/diff-server/util/version.v=$REPL_VERSION" -o build/repl-linux-amd64 ./cmd/repl
 
 # noms tool
 echo "Building noms..."
