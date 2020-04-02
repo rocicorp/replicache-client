38c7ee7d4c592caa20ed7ff3f740086471b676f7
Aaron Boodman <aaron@aaronboodman.com>
2020-04-01 14:46:46 -1000

    Remove noms from build

--- tool/release.sh
+++ tool/release.sh
@@ -15,10 +15,3 @@ REPL_VERSION=`git describe --tags`
 cd $ROOT
 GOOS=darwin GOARCH=amd64 go build -ldflags "-X roci.dev/diff-server/util/version.v=$REPL_VERSION" -o build/repl-darwin-amd64 ./cmd/repl
 GOOS=linux GOARCH=amd64 go build -ldflags "-X roci.dev/diff-server/util/version.v=$REPL_VERSION" -o build/repl-linux-amd64 ./cmd/repl
-
-# noms tool
-echo "Building noms..."
-NOMS_VERSION=`go mod graph | grep '^github.com/attic-labs/noms@' | cut -d' ' -f1 | head -n1`
-go get $NOMS_VERSION
-GOOS=darwin GOARCH=amd64 go build -o build/noms-darwin-amd64 github.com/attic-labs/noms/cmd/noms
-GOOS=linux GOARCH=amd64 go build -o build/noms-linux-amd64 github.com/attic-labs/noms/cmd/noms
